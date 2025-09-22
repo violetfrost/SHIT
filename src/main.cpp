c5e0c5b87fa6f011402f9c74d3541eb694be8697
Violet Frost <violetfrostbusiness@gmail.com>
2025-09-22 15:58:41 -0400

    make the templates actually work / recursive

--- src/main.cpp
+++ src/main.cpp
@@ -9,12 +9,63 @@
  * See LICENSE.md for details
  */
 
+// Side note, I really hate regex.
+// Like seriously, vehemently hate regex.
+// It works, though. Second try, too!
+static const std::regex CustomTagRegex(R"re(^\s*<%\s*(?:"([^"]+)"|([^\s>]+))\s*%>\s*$)re");
+// We want to set this up outside of a loop because... well, duh ^
+
 /// @brief Check whether or not a directory exists in the filesystem
 /// @param path The directory you want to checks
 /// @return True if the path exists, false if not.
 bool does_dir_exist(std::filesystem::path path)
 {
     return std::filesystem::exists(path);
+}
+
+/// @brief Compiles an HTML file with SHIT templates into a final deliverable
+/// @param path Path to the file you want to compile
+/// @return The compiled file, as a string array.
+std::vector<std::string> compile_html_file(std::string path)
+{
+    std::vector<std::string> compiledFile;
+
+    // Emergency breakout for if there's some filesystem fuckery going on
+    std::ifstream file(path);
+    if (!file.is_open())
+    {
+        std::cerr << "Failed to open file " << path << std::endl;
+        return compiledFile;
+    }
+
+    // Now we have access to the file and can actually begin to compile it
+    std::vector<std::string> exportLines;
+    std::string line;
+    while (std::getline(file, line))
+    {
+        std::smatch match;
+        if (std::regex_match(line, match, CustomTagRegex))
+        {
+            // We have a regex match! A custom tag was detected...
+            // If we didn't get a quoted or unquoted path, we continue the loop.
+            if (!match[1].matched && !match[2].matched)
+                continue;
+            
+            // This is probably really stupid code but it works. Convert relative paths from the tag into absolute directories.
+            std::string relativePath = match[1].matched ? match[1].str() : match[2].str();
+            std::string templatePath = path.substr(0, path.find_last_of("/\\") + 1) + relativePath;
+            std::vector<std::string> compiledTemplate = compile_html_file(templatePath); // Recursive!
+
+            // TODO do we want to add protections to prevent users from fork bombing themselves?
+            // Too lazy to do it right now.
+            
+            compiledFile.insert(compiledFile.end(), compiledTemplate.begin(), compiledTemplate.end());
+        } else {
+            compiledFile.push_back(line);
+        }
+    }
+
+    return compiledFile;
 }
 
 /// @brief Program entry point
@@ -70,41 +121,16 @@ int main(int argc, char *argv[])
         return 101;
     }
 
-    // Side note, I really hate regex.
-    // Like seriously, vehemently hate regex.
-    // It works, though. Second try, too!
-    static const std::regex customTagRegex(R"re(^\s*<%\s*(?:"([^"]+)"|([^\s>]+))\s*%>\s*$)re");
-    // We want to set this up outside of a loop because... well, duh ^
-
     // Iterate over all of the HTML filepaths in the input directory & its children
     for (int i = 0; i < htmlFiles.size(); i++)
     {
-        std::cout << htmlFiles[i] << std::endl;
+        std::vector<std::string> compiled = compile_html_file(htmlFiles[i]);
+        
+        std::cout << std::endl << std::endl << "HTML FILE GENERATED FROM " << htmlFiles[i] << std::endl << std::endl;
 
-        // Emergency breakout for if there's some filesystem fuckery going on
-        std::ifstream file(htmlFiles[i]);
-        if (!file.is_open())
+        for(int x = 0; x < compiled.size(); x++)
         {
-            std::cerr << "Failed to open file " << htmlFiles[i] << std::endl;
-            continue;
-        }
-
-        // Now we have access to the file and can actually begin to compile it
-        std::vector<std::string> exportLines;
-        std::string line;
-        while (std::getline(file, line))
-        {
-            std::smatch match;
-            if(std::regex_match(line, match, customTagRegex))
-            {
-                // We have a regex match! A custom tag was detected...
-                // Did we get a quoted path or an unquoted one?
-
-                if(match[1].matched)
-                    std::cout << match[1].str() << std::endl;
-                if(match[2].matched)
-                    std::cout << match[2].str() << std::endl;
-            }
+            std::cout  << compiled[x] << std::endl;
         }
     }
 
